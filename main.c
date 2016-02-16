31f9c43ade2e2e586b9fc9a9a5251acd1e2be904
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-02-16 21:13:14 +0200

    ---

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 #include "functions.h"
@@ -8,23 +7,4 @@ int main(int argc, char **argv)
     showMessage();    
     return 0;
 }
-=======
-/* 
- * File:   main.c
- * Author: wizard
- *
- * Created on February 16, 2016, 1:01 PM
- */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
-}
-
->>>>>>> f638a91721829f67625b5dfc15a0de23cdc980e5
