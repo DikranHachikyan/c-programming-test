ff1646e71151f220de34183a639863ec2a82f23f
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-02-16 22:27:35 +0200

    Меню с опции в main.c

--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 
 int main(int argc, char **argv)
 {
+    char choice=' ';
+
+    switch(choice)
+    {
+        case 'n': showMessage("New File\n");
+                  break;
+        case 'o': showMessage("Open File\n");
+                  break;
+        case 's': showMessage("Save File\n");
+                  break;
+        default: showMessage("Valid Options:n,o,s\n");
+    }
     showMessage("Hello C\n");    
     return 0;
 }
