f638a91721829f67625b5dfc15a0de23cdc980e5
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-02-16 13:04:36 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: wizard
+ *
+ * Created on February 16, 2016, 1:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
